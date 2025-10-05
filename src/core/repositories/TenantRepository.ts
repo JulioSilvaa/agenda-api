7fc9e34465d9e8d2d4ccdddb443903d37825ecfd
Julio Silvaa <juliocesar.amancio@yahoo.com.br>
2025-10-05 16:49:58 -0300

    fix: corrigir lógica de exclusão e atualização de inquilinos na implementação em memória

--- src/core/repositories/TenantRepository.ts
+++ src/core/repositories/TenantRepository.ts
@@ -3,10 +3,7 @@ import { TenantEntity } from "../entities/TenantEntity";
 export interface ITenantRepository {
   create(tenant: TenantEntity): Promise<TenantEntity>;
   findByEmail(email: string): Promise<TenantEntity | null>;
-<<<<<<< HEAD
-=======
   delete(id: string): Promise<void>;
   update(tenant: TenantEntity): Promise<TenantEntity>;
->>>>>>> 2e0b63285ebb1ae2c689f88422ff87c08feee2c3
   findById(id: string): Promise<TenantEntity | null>;
 }
--- src/infra/repositories/repositoryInMemory/TenantyRepositoryInMemory.ts
+++ src/infra/repositories/repositoryInMemory/TenantyRepositoryInMemory.ts
@@ -1,19 +1,36 @@
-import { TenantEntity } from '../../../core/entities/TenantEntity';
-import { ITenantRepository } from '../../../core/repositories/TenantRepository';
+import { TenantEntity } from "../../../core/entities/TenantEntity";
+import { ITenantRepository } from "../../../core/repositories/TenantRepository";
 
 export class TenantRepositoryInMemory implements ITenantRepository {
   private tenants: TenantEntity[] = [];
+
+  delete(id: string): Promise<void> {
+    const index = this.tenants.findIndex((t) => t.id === id);
+    if (index === -1) {
+      throw new Error("Tenant não encontrado");
+    }
+    this.tenants.splice(index, 1);
+    return Promise.resolve();
+  }
+  update(tenant: TenantEntity): Promise<TenantEntity> {
+    const index = this.tenants.findIndex((t) => t.id === tenant.id);
+    if (index === -1) {
+      throw new Error("Tenant não encontrado");
+    }
+    this.tenants[index] = tenant;
+    return Promise.resolve(tenant);
+  }
 
   async create(tenant: TenantEntity): Promise<TenantEntity> {
     this.tenants.push(tenant);
     return tenant;
   }
   async findByEmail(email: string): Promise<TenantEntity | null> {
-    const tenant = this.tenants.find(t => t.email === email);
+    const tenant = this.tenants.find((t) => t.email === email);
     return tenant ? tenant : null;
   }
   async findById(id: string): Promise<TenantEntity | null> {
-    const tenant = this.tenants.find(t => t.id === id);
+    const tenant = this.tenants.find((t) => t.id === id);
     return tenant ? tenant : null;
   }
 }
